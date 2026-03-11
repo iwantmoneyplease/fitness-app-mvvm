a83b1adbeddebbd3b1647aaf6a3b8f08a90b502f
iwantmoneyplease <122149540+iwantmoneyplease@users.noreply.github.com>
2026-03-11 15:49:49 +0100

    Added user class, along with subclasses. Does not correspond to issue connected. Whoopise, my bad. #2

---
+++ fitness-app-mvvm/Model/UserSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_app_mvvm.Model
+{
+    public class UserSettings : User
+    {
+        public UserSettings() : base(Type.UserSettings)
+        {
+
+        }
+        public override string GetDesc()
+        {
+            return "Subclass for the customizations and content (username, profile picture, etc.) of the user";
+        }
+
+        public override string ToString()
+        {
+            return $"return - UserSettings";
+        }
+    }
+}
