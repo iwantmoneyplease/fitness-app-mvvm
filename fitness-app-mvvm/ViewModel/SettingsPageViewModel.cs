49041b7791686639222cb806b4af9f10ab15248c
Viktor1009 <143164716+Viktor1009@users.noreply.github.com>
2026-03-11 17:27:50 +0100

    created a SettingsPage and a example CreatePage made by AI which is untestable due to other issues #4

---
+++ fitness-app-mvvm/ViewModel/SettingsPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+//Full Chatgpt kod
+//Detta är länken till konversationen: https://chatgpt.com/share/69b19551-bf10-8013-9e70-1f69e97d7a37
+namespace fitness_app_mvvm.ViewModel
+{
+    public class TypeSelectorViewModel
+    {
+        public ObservableCollection<Type> Types { get; } = new();
+        public ObservableCollection<string> Properties { get; } = new();
+
+        public Type SelectedType { get; set; }
+        public string SelectedProperty { get; set; }
+
+        public ICommand SelectTypeCommand { get; }
+        public ICommand SelectPropertyCommand { get; }
+
+        public TypeSelectorViewModel()
+        {
+            Types.Add(typeof(Person));
+            Types.Add(typeof(Product));
+
+            SelectTypeCommand = new Command<Type>(OnTypeSelected);
+            SelectPropertyCommand = new Command<string>(OnPropertySelected);
+        }
+
+        void OnTypeSelected(Type type)
+        {
+            SelectedType = type;
+
+            Properties.Clear();
+
+            var props = type.GetProperties();
+
+            foreach (var prop in props)
+                Properties.Add(prop.Name);
+        }
+
+        void OnPropertySelected(string property)
+        {
+            SelectedProperty = property;
+
+            // Here you now have BOTH values
+            SaveSelection();
+        }
+
+        void SaveSelection()
+        {
+            Console.WriteLine($"Type: {SelectedType.Name}, Sort: {SelectedProperty}");
+        }
+    }
+}
