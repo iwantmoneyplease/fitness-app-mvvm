522de6dfa2f204ff4b25438e61c6777ad57adb23
Viktor1009 <viktorosterberg10@gmail.com>
2026-03-25 09:57:14 +0100

    reverting deletion of user and workout

---
+++ fitness-app-mvvm/Model/User.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_app_mvvm.Model
+{
+    using System.Text.Json.Serialization;
+
+    namespace fitness_app_mvvm.Model
+    {
+        [JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
+        [JsonDerivedType(typeof(UserGoal), "user")]
+        [JsonDerivedType(typeof(UserHistory), "userhistory")]
+        [JsonDerivedType(typeof(UserSettings), "usersettings")]
+        public abstract class User
+        {
+            // Medlemsvariabler
+            public enum Type { UserGoal, UserHistory, UserSettings };
+            private Type userSection;
+            #region outdatedfunc
+            //private string registrationNumber = string.Empty;
+            //private string manufacturer = string.Empty;
+            //private string modelName = string.Empty;
+            //private int modelYear = 0;
+            #endregion
+
+            public abstract string GetDesc();
+
+            // Konstruktor (en metod med samma namn som klassen, som returnerar ett objekt)
+            protected User() { }
+            public User(Type userSection) // en konstruktor kan, men måste inte, ta parametrar
+            {
+                this.userSection = userSection;
+            }
+
+            public Type UserType
+            {
+                get { return userSection; }
+                set { userSection = value; }
+            }
+
+            #region outdatedCode
+            //// Get-Set för att hålla variablerna privata, och för att validera inkommande värden från UI (user interface, användargränssnittet)
+            //public string RegistrationNumber
+            //{
+
+            //}
+
+            ////TODO Tillverkare ska valideras, sparas i objektet och visas i UI
+            //public string ModelName
+            //{
+            //    get { return modelName; }
+            //    set 
+            //    {
+            //        if (!value.All(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)))
+            //        {
+            //            throw new ArgumentException("Modellfältet måste ha giltigt innehåll");
+            //        }
+            //        else
+            //        {
+            //            modelName = value;
+            //        }
+            //    }
+            //}
+
+            //public string ModelYear
+            //{
+            //    get { return modelYear.ToString(); }
+            //    set
+            //    {
+            //        if (int.TryParse(value, out int year))
+            //        {
+            //            if (year >= 1895 && year <= DateTime.Now.Year)
+            //            {
+            //                modelYear = year;
+            //            }
+            //            else
+            //            {
+            //                throw new ArgumentException("Året måste vara riktigt");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            throw new ArgumentException("Årfältet kan enbart ha siffror");
+            //        }
+            //    }
+            //}
+
+            ////TODO Modell ska valideras, sparas i objektet och visas i UI
+            //public string Manufacturer
+            //{
+            //    get { return manufacturer; }
+            //    set
+            //    {
+            //        if (!value.All(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)))
+            //        {
+            //            throw new ArgumentException("Tillverkarfältet måste ha giltigt innehåll");
+            //        }
+            //        else
+            //        {
+            //            manufacturer = value;
+            //        }
+
+            //    }
+            //}
+            #endregion
+        }
+    }
+}
---
+++ fitness-app-mvvm/Model/Workout.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace fitness_app_mvvm.Model
+{
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
+    [JsonDerivedType(typeof(WorkoutLeg), "leg")]
+    [JsonDerivedType(typeof(WorkoutCore), "core")]
+    [JsonDerivedType(typeof(WorkoutArm), "arm")]
+    public abstract class Workout
+    {
+        // Medlemsvariabler
+        public enum Type { Arm, Leg, Core };
+        private Type workoutType;
+        private string Quantity = string.Empty;
+        private string Time = string.Empty;
+        private string Sort = string.Empty;
+
+        public abstract string GetDesc();
+
+        // Konstruktor (en metod med samma namn som klassen, som returnerar ett objekt)
+        protected Workout() { }
+        public Workout(Type workoutType) // en konstruktor kan, men måste inte, ta parametrar
+        {
+            this.workoutType = workoutType;
+        }
+
+        public Type workoutType
+        {
+            get { return workoutType; }
+            set { workoutType = value; }
+        }
+        public string Quantity
+        {
+            get { return Quantity; }
+            set { Quantity = value; }
+        }
+        public string Time
+        {
+            get { return Time; }
+            set { Tíme = value; }
+        }
+        public string Sort
+        {
+            get { return Sort; }
+            set { Sort = value; }
+        }
+
+
+        #region RegistrationNumber
+        /*
+        // Get-Set för att hålla variablerna privata, och för att validera inkommande värden från UI (user interface, användargränssnittet)
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+
+            set
+            {
+                if (value.Length == 6)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (!char.IsLetter(value[i]))
+                            throw new ArgumentException("Inkorrekt registreringsnummer: De första tre tecknen måste vara bokstäver.");
+                    }
+
+                    for (int i = 3; i < 6; i++)
+                    {
+                        if (i < 5)
+                        {
+                            if (!char.IsDigit(value[i]))
+                                throw new ArgumentException("Inkorrekt registreringsnummer: Det fjärde och femte tecknet måste vara siffror.");
+                        }
+                        else
+                        {
+                            if (!char.IsDigit(value[i]) && !char.IsLetter(value[i]))
+                                throw new ArgumentException("Inkorrekt registreringsnummer: Det sjätte tecknet måste vara en siffra eller en bokstav.");
+                        }
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Ett registreringsnummer måste bestå av exakt 6 tecken, med tre bokstäver följt av två siffror och en siffra eller bokstav.");
+                }
+
+                registrationNumber = value.ToUpper();
+            }
+        }
+        */
+        #endregion
+        #region VehicleType
+        /*
+        // Fordonstyp tas in från dropdown-menyn, och behöver därför inte valideras
+        public Type VehicleType
+        {
+            get { return vehicleType; }
+            set { vehicleType = value; }
+        }
+        */
+        #endregion
+        #region ModelName
+        /*
+        //TODO Tillverkare ska valideras, sparas i objektet och visas i UI
+        public string ModelName
+        {
+            get { return modelName; }
+            set 
+            {
+                if (!value.All(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)))
+                {
+                    throw new ArgumentException("Modellfältet måste ha giltigt innehåll");
+                }
+                else
+                {
+                    modelName = value;
+                }
+            }
+        }
+        */
+        #endregion
+        #region ModelYear
+        /*
+        public string ModelYear
+        {
+            get { return modelYear.ToString(); }
+            set
+            {
+                if (int.TryParse(value, out int year))
+                {
+                    if (year >= 1895 && year <= DateTime.Now.Year)
+                    {
+                        modelYear = year;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Året måste vara riktigt");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Årfältet kan enbart ha siffror");
+                }
+            }
+        }
+        */
+        #endregion
+        #region Manufacturer 
+        /*
+        //TODO Modell ska valideras, sparas i objektet och visas i UI
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            set
+            {
+                manufacturer = value;
+            }
+        }
+        */
+        #endregion
+    }
+}
