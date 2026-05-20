a4b8c189e396e2b84a73c5aec4aebe0595cbdff4
iwantmoneyplease <122149540+iwantmoneyplease@users.noreply.github.com>
2026-05-20 11:01:25 +0200

    Buttons, not working

--- fitness-app-mvvm/ViewModel/GoalPageViewModel.cs
+++ fitness-app-mvvm/ViewModel/GoalPageViewModel.cs
@@ -1,6 +1,9 @@
-﻿using fitness_app_mvvm.Model;
+﻿using Android.Content;
+using fitness_app_mvvm;
+using fitness_app_mvvm.Model;
 using fitness_app_mvvm.Services;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
@@ -14,6 +17,7 @@ public class GoalPageViewModel : INotifyPropertyChanged
     private UserGoal currentGoal;
 
     public ObservableCollection<UserGoal> Goals => GoalService.Instance.GoalItems;
+    public ObservableCollection<UserGoal> CompletedGoals => GoalService.Instance.GoalItems;
 
     public ObservableCollection<string> SortOptions { get; } = new();
 
@@ -22,6 +26,9 @@ public class GoalPageViewModel : INotifyPropertyChanged
     public ICommand CoreCommand { get; }
     public ICommand SelectGoalCommand { get; }
     public ICommand SaveGoalCommand { get; }
+
+    public ICommand ShowUncompletedCommand { get; }
+    public ICommand ShowCompletedCommand { get; }
 
     private string selectedSort;
     public string SelectedSort
@@ -36,11 +43,17 @@ public class GoalPageViewModel : INotifyPropertyChanged
     public bool ShowSortOptions { get; set; }
     public bool ShowInput { get; set; }
 
+    public bool ShowUncompleted { get; set; }
+    public bool ShowCompleted { get; set; }
+
     public GoalPageViewModel()
     {
         ArmCommand = new Command(() => SelectGoal(new GoalArm()));
         LegCommand = new Command(() => SelectGoal(new GoalLeg()));
         CoreCommand = new Command(() => SelectGoal(new GoalCore()));
+
+        ShowUncompletedCommand = new Command(() => ShowUncompletedList());
+        ShowCompletedCommand = new Command(() => ShowCompletedList());
 
         SelectGoalCommand = new Command<string>(s =>
         {
@@ -73,16 +86,30 @@ public class GoalPageViewModel : INotifyPropertyChanged
         currentGoal.Time = Time;
         currentGoal.Quantity = Quantity;
 
-        Goals.Add(currentGoal);
+        App.CurrentUser.Goals.Add(currentGoal);
 
-        // reset
-        SelectedSort = null;
-        Time = string.Empty;
-        Quantity = string.Empty;
-        ShowInput = false;
+        ShowInput = false;  //UI reset
         ShowSortOptions = false;
-
         OnPropertyChanged(nameof(ShowInput));
-        OnPropertyChanged(nameof(ShowSortOptions));
+    }
+
+    private void ShowUncompletedList()
+    {
+
+    }
+    private void ShowCompletedList()
+    {
+
+    }
+
+    private void CompareGoal(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        foreach (var Goal in Goals.ToList())
+        {
+            if (!completedGoal.Contains(Goal))
+            {
+
+            }
+        }
     }
 }
